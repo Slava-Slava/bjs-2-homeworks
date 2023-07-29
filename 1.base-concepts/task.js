337d2e9533f5b7d9435839906e33774b18fa8387
Slava Vasiliev <sb-fantom@yandex.ru>
2023-07-29 16:37:04 +0300

    README.md

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = Math.pow(b, 2) - 4 * a * c;
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		let root1 = -b / (2 * a);
+		arr.push(root1);
+		return arr;
+	} else if (discriminant > 0) {
+		let root2 = (-b + Math.sqrt(d)) / (2 * a);
+		let root3 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root2, root3);
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	if (Number.isNaN(percent) || Number.isNaN(contribution) || Number.isNaN(amount) || Number.isNaN(countMonths)) {
+		return false
+	} else {
+		let interestRate = percent / 100 / 12;
+		let loanBody = amount - contribution;
+		let months = countMonths;
+		let monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** months) - 1)))
+		let totalAmount = (monthlyPayment * countMonths).toFixed(2);
+		return parseInt(totalAmount);
+	}
 }
