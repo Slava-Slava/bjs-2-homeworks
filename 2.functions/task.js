e5daa1310062c181ae74ea30ea6887a7dc9d8db2
Slava Vasiliev <sb-fantom@yandex.ru>
2023-07-29 21:57:24 +0300

    README.md

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,22 @@
 function getArrayParams(...arr) {
+//  let min = Infinity;
+//  let max = -Infinity;
+  let min = [0];
+  let max = [0];
+  let sum = 0;
+  let avg = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+          max = arr[i]
+      }
+      else if (arr[i] < min) {
+          min = arr[i]
+      }
+          sum += arr[i];
+  }
+  avg = parseInt(arr.length ? sum / arr.length:undefined.toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
 
