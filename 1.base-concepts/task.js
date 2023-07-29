217ecc1a0d2dff1cd47435ea50663d1dc400bd90
Slava Vasiliev <sb-fantom@yandex.ru>
2023-07-29 18:01:41 +0300

    README11.md

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,10 +18,10 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-        let P = percent / 100 / 12;
-		let S = amount - contribution;
-		let n = countMonths;
-        let payment = S * (P + (P / (((1 + P) ** n) - 1)));
-		let totalAmount = (payment * n).toFixed(2);
-		return parseInt(totalAmount);
+	let P = percent/100/12;
+	let S = amount - contribution;
+	let n = countMonths;
+	let payment = S * (P + (P / (((1 + P)** n) - 1)));
+	let totalSum = (payment * n).toFixed(2);
+	return totalSum;
 }
