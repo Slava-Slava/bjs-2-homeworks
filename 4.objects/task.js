b319bd8f3d5bf544dbe9608e9e4751e714942b8b
Slava Vasiliev <sb-fantom@yandex.ru>
2023-08-03 19:02:39 +0300

    README11.md

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,30 +5,30 @@ function Student(name, gender, age) {
 	this.marks = [];
 
 
-this.setSubject = function(subjectName) {
-	this.subject = subjectName
-}
-
-this.addMarks = function(...marksToAdd) {
-	if (!this.marks) {
-		return;
-	} else {
-		this.marks.push(...marksToAdd);
+	this.setSubject = function(subjectName) {
+		this.subject = subjectName
 	}
-}
 
-this.getAverage = function() {
-	if (!this.marks || this.marks.length) {
-		return 0;
-	} else {
-		const sum = this.marks.reduce((acc, curr) => acc + curr);
-		return sum / this.marks.length;
+	this.addMarks = function(...marksToAdd) {
+		if (!this.marks) {
+			return;
+		} else {
+			this.marks.push(...marksToAdd);
+		}
 	}
-}
 
-this.exclude = function(reason) {
-	delete this.subject;
-	delete this.marks;
-	this.excluded = reason;
-}
+	this.getAverage = function() {
+		if (!this.marks || this.marks.length) {
+			return 0;
+		} else {
+			const sum = this.marks.reduce((acc, curr) => acc + curr);
+			return sum / this.marks.length;
+		}
+	}
+
+	this.exclude = function(reason) {
+		delete this.subject;
+		delete this.marks;
+		this.excluded = reason;
+	}
 }
