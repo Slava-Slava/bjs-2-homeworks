bc0a509cbafac9cc2b89cb309f525d86b07bc64f
Slava Vasiliev <sb-fantom@yandex.ru>
2023-08-06 14:04:33 +0300

    README.md

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -54,3 +54,27 @@ class DetectiveBook extends Book{
 		this.type = "detective";
 	}
 }
+class Library{
+    constructor(name){
+        this.name=name;
+        this.books=[];
+    }
+    addBook(book){
+        if (book.state>30){
+            this.books.push(book);
+        }
+    }
+    findBookBy(type, value){
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i][type] === value) return this.books[i];
+        }
+        return null;
+    }
+    giveBookByName(bookName){
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) return this.books.splice([i],1)[0];
+        }
+        return null;
+
+    }
+}
